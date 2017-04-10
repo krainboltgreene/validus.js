d059cdb9f80a8b03180d8a3c9104a5e571f82b2d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-09 17:43:18 -0700

    Optimizing

--- source/validate/index.js
+++ source/validate/index.js
@@ -4,8 +4,10 @@ import validated from "../validated"
 
 export default function validate (validations: ValidationsType): Function {
   return function validateWithValidations (scope: ScopeType): Function {
+    const property = prop(scope)(validations)
+
     return function validateWithValidationsAndScope (subject: SubjectType): Array<MessageType> {
-      return validated(subject)(prop(scope, validations))
+      return validated(subject)(property)
     }
   }
 }
--- source/validates/index.js
+++ source/validates/index.js
@@ -1,11 +1,10 @@
-import {isEmpty} from "ramda"
 import {map} from "ramda"
-import {reject} from "ramda"
+import {compact} from "ramda-extra"
 
 import validated from "../validated"
 
 export default function validates (validations: ValidationsType): Function {
   return function validatesWithValidations (subject: SubjectType): ErrorsType {
-    return reject(isEmpty, map(validated(subject), validations))
+    return compact(map(validated(subject))(validations))
   }
 }
